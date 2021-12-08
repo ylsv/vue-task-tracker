d6a805c044e76ba018728814e170bc68c7a8029e
Юрий Лисов <yulisov@fil-it.ru>
2021-12-08 19:02:45 +0300

    create app, add app.js, header, button and tasks components, ability to delete tasks

---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <Task :task="task" @delete-task="$emit('delete-task', task.id)" />
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+export default {
+  name: 'Tasks',
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ['delete-task']
+}
+</script>
